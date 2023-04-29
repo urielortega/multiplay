1a7fd1a43f68dcc4f480a57cc5c50e22fa017d7a
Uriel Ortega <urielortega2509@gmail.com>
2023-04-29 12:38:07 -0600

    Initial Commit

---
+++ MultiPlay/MultiPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPlayApp.swift
+//  MultiPlay
+//
+//  Created by Uriel Ortega on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
